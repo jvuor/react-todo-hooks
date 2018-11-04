1f5450155fc6fdfd641c0e96b92cda9aeb547e88
jvuor <jussi.vuorinen@gmail.com>
2018-11-04 14:34:06 +0200

    Updates

---
+++ precache-manifest.2d934b9c5544ada78f20fa9f1dc6a82d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "407c8ba4da9e7f1ecfff",
+    "url": "/react-todo-hooks/static/js/runtime~main.407c8ba4.js"
+  },
+  {
+    "revision": "dc296f04b315d4bf7b25",
+    "url": "/react-todo-hooks/static/js/main.dc296f04.chunk.js"
+  },
+  {
+    "revision": "72d47cd97937fff1aadb",
+    "url": "/react-todo-hooks/static/js/1.72d47cd9.chunk.js"
+  },
+  {
+    "revision": "dc296f04b315d4bf7b25",
+    "url": "/react-todo-hooks/static/css/main.304042f5.chunk.css"
+  },
+  {
+    "revision": "9d87b8578e1ba7f31e48f6dcf523c0c5",
+    "url": "/react-todo-hooks/index.html"
+  }
+];
